3a36ea9ff22a8b72087e27993bea9d93672c0118
Khoa <ovn6002@gmail.com>
2023-06-21 11:35:58 +0700

    cc

---
+++ Array-Hashing/array_concatenation.cpp
@@ -0,0 +1,13 @@
+#include "lib.h"
+
+class Solution 
+{
+    public:
+        vector<int> getConcatenation(vector<int> & nums) 
+        {
+            int size = nums.size();
+            for (int i = 0; i < size; i++)
+                nums.push_back(nums[i]);
+            return nums;
+        }
+};
