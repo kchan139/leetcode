a605c4a5376fab4bd3aca299907ef007ab6b212f
Khoa <ovn6002@gmail.com>
2023-06-21 16:30:02 +0700

    add new solution

---
+++ Two Pointers/merge_string.cpp
@@ -0,0 +1,35 @@
+#include "lib.h"
+
+string mergeAlternately(string word1, string word2) 
+{
+    string result = "";
+    int i = 0, j = 0;
+    int len1 = word1.length(), len2 = word2.length();
+
+    while (i < len1 && j < len2)
+    {
+        result += word1[i] + word2[j];
+        i++; j++;    
+    }
+
+    while (i < len1)
+    {
+        result += word1[i];
+        i++;
+    }
+    while (j < len2)
+    {
+        result += word2[j];
+        j++;
+    }
+
+    return result;
+}
+
+int main ()
+{
+    string w1 = "abcd";
+    string w2 = "pq";
+
+    cout << mergeAlternately(w1, w2);
+}
