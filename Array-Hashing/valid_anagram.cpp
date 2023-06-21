4b12e595022db3e03065bc51e782e6b40c94b407
Khoa <ovn6002@gmail.com>
2023-06-21 11:34:43 +0700

    cc

---
+++ Array-Hashing/valid_anagram.cpp
@@ -0,0 +1,12 @@
+#include "lib.h"
+
+class Solution 
+{
+    public:
+        bool isAnagram(string s, string t) 
+        {
+            sort(s.begin(), s.end());
+            sort(t.begin(), t.end());
+            return s == t;
+        }
+};
