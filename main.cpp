0c9cc9e6182b9f1eecb057ad3a8e76c8f6a7a230
Khoa <ovn6002@gmail.com>
2023-06-22 21:24:25 +0700

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include "Array-Hashing/group_anagrams.cpp"
+
+int main()
+{
+    // int n;
+    // cout << "Enter the number of strings: ";
+    // cin >> n;
+
+    // vector<string> strs(n);
+    // cout << "Enter the strings:" << endl;
+    // for (int i = 0; i < n; ++i)
+    // {
+    //     cin >> strs[i];
+    // }
+
+    vector<string> strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
+
+    Solution solution;
+    vector<vector<string>> groupedAnagrams = solution.groupAnagrams(strs);
+
+    cout << "Grouped Anagrams:" << endl;
+    for (const auto& group : groupedAnagrams)
+    {
+        cout << "[ ";
+        for (const auto& str : group)
+            cout << str << " ";
+        cout << "]" << endl;
+    }
+
+    return 0;
+}
